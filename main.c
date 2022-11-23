1f72197ad736469208dee022991ee7512c8844fa
Bernardino Villagra <bernardinovillagra@hotmail.com>
2022-11-23 18:50:04 -0300

    Todas las operaciones matematicas ya estan funcionando

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <math.h>
 
+double potencia(double num1, double num2){
+  return pow(num1, num2);
+}
+
+int raiz(int num1){
+  return sqrt(num1);
+}
+
 int suma(int num1, int num2) {
   return num1 + num2;
 }
@@ -19,6 +27,7 @@ int division(int num1, int num2) {
 
 int main() {
   int operacion, numero1, numero2;
+  double pnumero1, pnumero2;
 
   printf("**************************************************\n");
   printf("*********** CALCULDORA EN LENGUAJE C *************\n");
@@ -32,36 +41,82 @@ int main() {
   printf("5) SUMA\n");
   printf("6) RESTA\n");
 
+  // Solicitamos el tipo de operacion
   printf("INGRESE EL NUMERO DE OPERACION: ");
   scanf("%d", &operacion);
 
-  printf("INGRESE EL PRIMER NUMERO: ");
-  scanf("%d", &numero1);
+  // // Solicitamos los numeros
+  // printf("INGRESE EL PRIMER NUMERO: ");
+  // scanf("%d", &numero1);
 
-  printf("INGRESE EL SEGUNDO NUMERO: ");
-  scanf("%d", &numero2);
+  // printf("INGRESE EL SEGUNDO NUMERO: ");
+  // scanf("%d", &numero2);
 
   switch( operacion ){
     case 1:
-    printf("potencia");
-    break;
+      // Solicitamos los numeros
+      printf("INGRESE EL VALOR DE LA BASE: ");
+      scanf("%lf", &pnumero1);
+
+      printf("INGRESE EL VALOR DEL EXPONENTE: ");
+      scanf("%lf", &pnumero2);
+
+      printf("LA POTENCIA ES: %.2lf", potencia(pnumero1, pnumero2));
+      break;
+
     case 2:
-    printf("raiz");
-    break;
+      // Solicitamos los numeros
+      printf("INGRESE EL NUMERO: ");
+      scanf("%d", &numero1);
+      printf("LA RAIZ ES: %d", raiz(numero1));
+      break;
+
     case 3:
-    printf("LA MULTIPLICACION ES: %d", multiplicacion(numero1, numero2));
-    break;
+      // Solicitamos los numeros
+      printf("INGRESE EL PRIMER NUMERO: ");
+      scanf("%d", &numero1);
+
+      printf("INGRESE EL SEGUNDO NUMERO: ");
+      scanf("%d", &numero2);
+
+      printf("LA MULTIPLICACION ES: %d", multiplicacion(numero1, numero2));
+      break;
+
     case 4:
-    printf("LA DIVISION ES: %d", division(numero1, numero2));
-    break;
+      // Solicitamos los numeros
+      printf("INGRESE EL PRIMER NUMERO: ");
+      scanf("%d", &numero1);
+
+      printf("INGRESE EL SEGUNDO NUMERO: ");
+      scanf("%d", &numero2);
+
+      printf("LA DIVISION ES: %d", division(numero1, numero2));
+      break;
+
     case 5:
-    printf("LA SUMA ES: %d", suma(numero1, numero2));
-    break;
+      // Solicitamos los numeros
+      printf("INGRESE EL PRIMER NUMERO: ");
+      scanf("%d", &numero1);
+
+      printf("INGRESE EL SEGUNDO NUMERO: ");
+      scanf("%d", &numero2);
+
+      printf("LA SUMA ES: %d", suma(numero1, numero2));
+      break;
+
     case 6:
-    printf("LA RESTA ES: %d", resta(numero1, numero2));
-    break;
+      // Solicitamos los numeros
+      printf("INGRESE EL PRIMER NUMERO: ");
+      scanf("%d", &numero1);
+
+      printf("INGRESE EL SEGUNDO NUMERO: ");
+      scanf("%d", &numero2);
+
+      printf("LA RESTA ES: %d", resta(numero1, numero2));
+      break;
+
     default:
-    printf("opcion incorrecta");
+      printf("opcion incorrecta");
   }
 
   return 0;
